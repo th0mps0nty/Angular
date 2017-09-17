a775e3257ed853d073f651d449f845c0443bf7f9
Tyler Thompson <tyler.thompson@tinnacity.com>
2017-09-16 20:07:39 -0400

    assignment 9 complete

--- app/scripts/directivea/seekBar.js
+++ app/scripts/directivea/seekBar.js
@@ -1,35 +1,42 @@
 (function() {
-  function seekBar($document) {
-    var calculatePercent = function(seekBar, event) {
-      var offsetX = event.pageX - seekBar.offset().left;
-      var seekBarWidth = seekBar.width();
-      var offsetXPercent = offsetX / seekBarWidth;
-      offsetXPercent = Math.max(0, offsetXPercent);
-      offsetXPercent = Math.min(1, offsetXPercent);
-      return offsetXPercent;
-    };
+    function seekBar($document) {
+      var calculatePercent = function(seekBar, event) {
+        var offsetX = event.pageX - seekBar.offset().left;
+        var seekBarWidth = seekBar.width();
+        var offsetXPercent = offsetX / seekBarWidth;
+        offsetXPercent = Math.max(0, offsetXPercent);
+        offsetXPercent = Math.min(1, offsetXPercent);
+        return offsetXPercent;
+      };
 
-    return {
-      templateUrl: '/templates/directives/seek_bar.html',
-      replace: true,
-      restrict: 'E',
-      scope: {},
-      link: function(scope, element, attributes) {
-        scope.value = 0;
-        scope.max = 100;
+      return {
+        templateUrl: '/templates/directives/seek_bar.html',
+        replace: true,
+        restrict: 'E',
+        scope: {},
+        link: function(scope, element, attributes) {
+          scope.value = 0;
+          scope.max = 100;
 
-        var seekBar = $(element);
+          var seekBar = $(element);
 
-        var percentString = function() {
-          var value = scope.value;
-          var max = scope.max;
-          var percent = value / max * 100;
-          return percent + "%";
-        };
+          var percentString = function() {
+            var value = scope.value;
+            var max = scope.max;
+            var percent = value / max * 100;
+            return percent + "%";
+          };
 
-        scope.fillStyle = function() {
-          return {
-            width: percentString()
+          scope.fillStyle = function() {
+            return {
+              width: percentString()
+            };
+          };
+
+          scope.thumbStyle = function() {
+            return {
+              left: percentString()
+            };
           };
 
           scope.onClickSeekBar = function(event) {
@@ -56,6 +63,6 @@
   }
 
   angular
-    .module('blocJams')
-    .directive('seekBar', ['$document', seekBar]);
+  .module('blocJams')
+  .directive('seekBar', ['$document', seekBar]);
 })();
